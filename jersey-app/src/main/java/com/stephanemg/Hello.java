fdd96351b83e56d2ccd9d1cc0a3edb3451db35f5
stephanemg <stephane.mg@gmail.com>
2020-10-07 05:40:41 +0200

    i

---
+++ jersey-app/src/main/java/com/stephanemg/Hello.java
@@ -0,0 +1,39 @@
+package com.stephanemg;
+
+public class Hello {
+	String id;
+	String message;
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String toString() {
+		return "Hello[id:" + this.id + " message:" + this.message + "]";
+	}
+
+}
--- jersey-app/src/main/java/com/stephanemg/MyJerseyPage.java
+++ jersey-app/src/main/java/com/stephanemg/MyJerseyPage.java
@@ -1,6 +1,8 @@
 package com.stephanemg;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -11,5 +13,12 @@ public class MyJerseyPage {
 	@Produces(MediaType.TEXT_HTML)
 	public String sayHtmlHello() {
 		return "Hello from Jersey";
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String sayHtmlHello(Hello hello) {
+		return hello.toString();
 	}
 }
