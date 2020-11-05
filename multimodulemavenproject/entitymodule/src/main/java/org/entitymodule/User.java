efbe8fadb68c39adbbfeb34d2f6d89b1e27b3b35
stephanemg <stephane.mg@gmail.com>
2020-11-05 06:56:12 +0100

    g

--- multimodulemavenproject/entitymodule/src/main/java/org/entitymodule/User.java
+++ multimodulemavenproject/entitymodule/src/main/java/org/entitymodule/User.java
@@ -5,11 +5,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class User {
 	String name;
 
